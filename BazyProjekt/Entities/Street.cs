e29d64616d390a4c7d3f36eed3ddd215167135f4
KarolSzydlak <252560@student.pwr.edu.pl>
2024-01-08 17:52:22 +0100

    parę metod admina

--- BazyProjekt/Entities/Street.cs
+++ BazyProjekt/Entities/Street.cs
@@ -9,7 +9,9 @@ namespace BazyProjekt.Entities
     internal class Street
     {
         private Int32 id_city { get; set; }
+        public Int32 Id_city { get { return id_city; } set { id_city = value; } }
         private String name { get; set; }
+        public String Name { get { return name; } set { name = value; } }
 
     }
 }
